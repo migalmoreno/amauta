1722abf1960600f9d645286f38a4208a53d24c24
Miguel Ángel Moreno <mail@migalmoreno.com>
2026-08-10 17:29:06 +0200

    Format user ns

--- dev/user.clj
+++ dev/user.clj
@@ -1,16 +1,20 @@
 (ns user
-  (:require [blog.site :as site]
-            [blog.ssg.core :as core]
-            [blog.ssg.serve :as serve]
-            [shadow.cljs.devtools.api :as shadow]
-            [shadow.cljs.devtools.server :as shadow-server]))
+  (:require
+   [blog.site :as site]
+   [blog.ssg.core :as core]
+   [blog.ssg.serve :as serve]
+   [shadow.cljs.devtools.api :as shadow]
+   [shadow.cljs.devtools.server :as shadow-server]))
 
-(defn build! []
+(defn build!
+  []
   (require 'blog.site :reload)
   (core/build! site/site))
-(defn stop!  [] (serve/stop!) (shadow-server/stop!))
 
-(defn serve! [& [opts]]
+(defn stop! [] (serve/stop!) (shadow-server/stop!))
+
+(defn serve!
+  [& [opts]]
   (shadow-server/start!)
   (shadow/watch :frontend)
   (serve/serve! build! (merge {:dirs ["posts" "assets/css" "src"]} opts)))
